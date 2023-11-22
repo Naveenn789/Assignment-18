ba39666ee9f3eb52f37da17dace46e0be88cd284
Naveenn789 <naveengalla77@gmail.com>
2023-11-22 16:11:38 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment18
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop laptop = new Laptop();
+            IDisplayable one = laptop;
+            IRechargeable two = laptop;
+            IConnectable connectable = laptop;
+            connectable.Connect(true);
+            two.Charge(0);
+            one.Display();
+            Smartphone phone = new Smartphone();
+            IConnectable phone1 = phone;
+            phone1.Connect(true);
+            IDisplayable phone2 = phone;
+            phone.Display();
+            IRechargeable phone3 = phone;
+            phone3.Charge(0);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Smartphone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment18
+{
+    internal class Smartphone : IConnectable, IRechargeable, IDisplayable
+
+    {
+        int charge;
+        DateTime purchaseDate;
+        bool connect;
+        double price;
+
+        public void Charge(int minute)
+        {
+            Console.WriteLine("Enter The device charging in minutes");
+            charge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the price for the device");
+            double price = double.Parse(Console.ReadLine());
+            purchaseDate = DateTime.Now;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Charg of the device {charge} minutes" + charge);
+            Console.WriteLine("Price of the device : " + price);
+            Console.WriteLine("Purchased Date of the device: " + purchaseDate);
+        }
+        void IConnectable.Connect(bool v)
+        {
+            Console.WriteLine("Is the device is connected?");
+            connect = bool.Parse(Console.ReadLine());
+
+        }
+
+
+    }
+}
